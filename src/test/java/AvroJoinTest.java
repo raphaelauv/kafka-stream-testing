a95dbaa80a30e8a60dc668a01c27652398ceaf73
raphaelauv <raphaelauv@gmail.com>
2021-02-10 20:22:02 +0100

    [FEATURE] kafka 2.7

--- src/test/java/AvroJoinTest.java
+++ src/test/java/AvroJoinTest.java
@@ -4,7 +4,7 @@ import java.util.Properties;
 import example.avro.Car;
 import example.avro.Color;
 import example.avro.Order;
-import io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
 import io.confluent.kafka.streams.serdes.avro.SpecificAvroSerde;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
@@ -19,90 +19,90 @@ import org.junit.jupiter.api.Test;
 
 import static java.util.Collections.singletonMap;
 
-import static io.confluent.kafka.serializers.AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG;
+import static io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class AvroJoinTest {
 
-  final String INPUT_TOPIC_CAR   = "in_car";
-  final String INPUT_TOPIC_COLOR = "in_color";
-  final String OUTPUT_TOPIC      = "out";
+    final String INPUT_TOPIC_CAR = "in_car";
+    final String INPUT_TOPIC_COLOR = "in_color";
+    final String OUTPUT_TOPIC = "out";
 
 
-  private TopologyTestDriver testDriver;
+    private TopologyTestDriver testDriver;
 
 
-  private TestInputTopic<String, Car>    inputTopicCar;
-  private TestInputTopic<String, Color>  inputTopicColor;
-  private TestOutputTopic<String, Order> outputTopic;
+    private TestInputTopic<String, Car> inputTopicCar;
+    private TestInputTopic<String, Color> inputTopicColor;
+    private TestOutputTopic<String, Order> outputTopic;
 
 
-  private static final String SCHEMA_REGISTRY_SCOPE    = AvroJoinTest.class.getName();
-  private static final String MOCK_SCHEMA_REGISTRY_URL = "mock://" + SCHEMA_REGISTRY_SCOPE;
+    private static final String SCHEMA_REGISTRY_SCOPE = AvroJoinTest.class.getName();
+    private static final String MOCK_SCHEMA_REGISTRY_URL = "mock://" + SCHEMA_REGISTRY_SCOPE;
 
-  @BeforeEach
-  public void setup() {
+    @BeforeEach
+    public void setup() {
 
-    Properties props = new Properties();
-    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "test");
-    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
-    props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
-    props.put(AbstractKafkaAvroSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "test");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
+        props.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, SpecificAvroSerde.class);
+        props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
 
-    // Create Serdes used for test record keys and values
-    Serde<String> stringSerde = Serdes.String();
-    Serde<Color> avroColorSerde = new SpecificAvroSerde<>();
-    Serde<Car> avroCarSerde = new SpecificAvroSerde<>();
-    Serde<Order> avroOderSerde = new SpecificAvroSerde<>();
+        // Create Serdes used for test record keys and values
+        Serde<String> stringSerde = Serdes.String();
+        Serde<Color> avroColorSerde = new SpecificAvroSerde<>();
+        Serde<Car> avroCarSerde = new SpecificAvroSerde<>();
+        Serde<Order> avroOderSerde = new SpecificAvroSerde<>();
 
-    Map<String, String> config = singletonMap(SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
-    avroColorSerde.configure(config, false);
-    avroCarSerde.configure(config, false);
-    avroOderSerde.configure(config, false);
+        Map<String, String> config = singletonMap(SCHEMA_REGISTRY_URL_CONFIG, MOCK_SCHEMA_REGISTRY_URL);
+        avroColorSerde.configure(config, false);
+        avroCarSerde.configure(config, false);
+        avroOderSerde.configure(config, false);
 
-    StreamsBuilder builder = new StreamsBuilder();
+        StreamsBuilder builder = new StreamsBuilder();
 
-    KTable<String, Car> cars = builder.table(INPUT_TOPIC_CAR, Consumed.with(stringSerde, avroCarSerde, null, null));
+        KTable<String, Car> cars = builder.table(INPUT_TOPIC_CAR, Consumed.with(stringSerde, avroCarSerde, null, null));
 
-    KStream<String, Color> colors = builder.stream(INPUT_TOPIC_COLOR, Consumed.with(stringSerde, avroColorSerde, null, null));
+        KStream<String, Color> colors = builder.stream(INPUT_TOPIC_COLOR, Consumed.with(stringSerde, avroColorSerde, null, null));
 
-    colors.join(cars, (color, car) -> Order.newBuilder().setCarName(car.getBrand()).setColorName(color.getName()).build())
-          .to(OUTPUT_TOPIC, Produced.with(stringSerde, avroOderSerde));
+        colors.join(cars, (color, car) -> Order.newBuilder().setCarName(car.getBrand()).setColorName(color.getName()).build())
+                .to(OUTPUT_TOPIC, Produced.with(stringSerde, avroOderSerde));
 
-    testDriver = new TopologyTestDriver(builder.build(), props);
+        testDriver = new TopologyTestDriver(builder.build(), props);
 
-    inputTopicCar = testDriver.createInputTopic(INPUT_TOPIC_CAR, stringSerde.serializer(), avroCarSerde.serializer());
+        inputTopicCar = testDriver.createInputTopic(INPUT_TOPIC_CAR, stringSerde.serializer(), avroCarSerde.serializer());
 
-    inputTopicColor = testDriver.createInputTopic(INPUT_TOPIC_COLOR, stringSerde.serializer(), avroColorSerde.serializer());
+        inputTopicColor = testDriver.createInputTopic(INPUT_TOPIC_COLOR, stringSerde.serializer(), avroColorSerde.serializer());
 
-    outputTopic = testDriver.createOutputTopic(OUTPUT_TOPIC, stringSerde.deserializer(), avroOderSerde.deserializer());
+        outputTopic = testDriver.createOutputTopic(OUTPUT_TOPIC, stringSerde.deserializer(), avroOderSerde.deserializer());
 
-  }
+    }
 
-  @AfterEach
-  public void tearDown() {
-    testDriver.close();
-  }
+    @AfterEach
+    public void tearDown() {
+        testDriver.close();
+    }
 
-  @Test
-  public void shouldJoinByKeys() {
+    @Test
+    public void shouldJoinByKeys() {
 
-    inputTopicCar.pipeInput("21", Car.newBuilder().setBrand("honda").build());
-    inputTopicCar.pipeInput("35", Car.newBuilder().setBrand("mercedes").build());
+        inputTopicCar.pipeInput("21", Car.newBuilder().setBrand("honda").build());
+        inputTopicCar.pipeInput("35", Car.newBuilder().setBrand("mercedes").build());
 
-    inputTopicColor.pipeInput("21", Color.newBuilder().setName("red").build());
-    inputTopicColor.pipeInput("35", Color.newBuilder().setName("blue").build());
+        inputTopicColor.pipeInput("21", Color.newBuilder().setName("red").build());
+        inputTopicColor.pipeInput("35", Color.newBuilder().setName("blue").build());
 
-    assertThat(outputTopic.getQueueSize()).isEqualTo(2L);
+        assertThat(outputTopic.getQueueSize()).isEqualTo(2L);
 
-    outputTopic.readValuesToList().forEach(order -> {
-      if (order.getColorName().toString().equals("red")) {
-        assertThat(order.getCarName().toString()).isEqualTo("honda");
-      } else {
-        assertThat(order.getCarName().toString()).isEqualTo("mercedes");
-      }
-    });
+        outputTopic.readValuesToList().forEach(order -> {
+            if (order.getColorName().toString().equals("red")) {
+                assertThat(order.getCarName().toString()).isEqualTo("honda");
+            } else {
+                assertThat(order.getCarName().toString()).isEqualTo("mercedes");
+            }
+        });
 
-  }
+    }
 
 }
--- src/test/java/WindowTest.java
+++ src/test/java/WindowTest.java
@@ -19,91 +19,91 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class WindowTest {
 
-  final String INPUT_TOPIC  = "in";
-  final String OUTPUT_TOPIC = "out";
+    final String INPUT_TOPIC = "in";
+    final String OUTPUT_TOPIC = "out";
 
-  private TopologyTestDriver               testDriver;
-  private TestInputTopic<String, Integer>  inputTopic;
-  private TestOutputTopic<String, Integer> outputTopic;
+    private TopologyTestDriver testDriver;
+    private TestInputTopic<String, Integer> inputTopic;
+    private TestOutputTopic<String, Integer> outputTopic;
 
 
-  @BeforeEach
-  public void setup() {
+    @BeforeEach
+    public void setup() {
 
-    Properties props = new Properties();
-    props.put(StreamsConfig.APPLICATION_ID_CONFIG, "test");
-    props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
-    props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
-    try {
-      props.put(StreamsConfig.STATE_DIR_CONFIG,
-        Files.createTempDirectory("tumbling-windows").toAbsolutePath().toString());
-    } catch (IOException e) {
+        Properties props = new Properties();
+        props.put(StreamsConfig.APPLICATION_ID_CONFIG, "test");
+        props.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, "dummy:1234");
+        props.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, 0);
+        try {
+            props.put(StreamsConfig.STATE_DIR_CONFIG,
+                    Files.createTempDirectory("tumbling-windows").toAbsolutePath().toString());
+        } catch (IOException e) {
+
+        }
+
+        StreamsBuilder builder = new StreamsBuilder();
+
+        TimeWindows windows = TimeWindows
+                .of(Duration.ofSeconds(1))
+                .advanceBy(Duration.ofSeconds(1))
+                .grace(Duration.ofMillis(150L)); //a grace is necessary for suppress
+
+        builder.stream(INPUT_TOPIC, Consumed.with(new Serdes.StringSerde(), new Serdes.IntegerSerde(), null, null))
+                .groupByKey()
+                .windowedBy(windows)
+                .count()
+                .suppress(Suppressed.untilWindowCloses(unbounded()))
+                .mapValues((readOnlyKey, value) -> value.intValue())
+                .toStream((key, value) -> key.key())
+                .to(OUTPUT_TOPIC, Produced.with(new Serdes.StringSerde(), new Serdes.IntegerSerde()));
+
+        testDriver = new TopologyTestDriver(builder.build(), props);
+
+        inputTopic = testDriver.createInputTopic(INPUT_TOPIC, new StringSerializer(), new IntegerSerializer());
+        outputTopic = testDriver.createOutputTopic(OUTPUT_TOPIC, new StringDeserializer(), new IntegerDeserializer());
 
     }
 
-    StreamsBuilder builder = new StreamsBuilder();
+    @AfterEach
+    public void tearDown() {
+        testDriver.close();
+    }
 
-    TimeWindows windows = TimeWindows
-      .of(Duration.ofSeconds(1))
-      .advanceBy(Duration.ofSeconds(1))
-      .grace(Duration.ofMillis(150L)); //a grace is necessary for suppress
+    @Test
+    public void shouldWindow() {
 
-    builder.stream(INPUT_TOPIC, Consumed.with(new Serdes.StringSerde(), new Serdes.IntegerSerde(), null, null))
-           .groupByKey()
-           .windowedBy(windows)
-           .count()
-           .suppress(Suppressed.untilWindowCloses(unbounded()))
-           .mapValues((readOnlyKey, value) -> value.intValue())
-           .toStream((key, value) -> key.key())
-           .to(OUTPUT_TOPIC, Produced.with(new Serdes.StringSerde(), new Serdes.IntegerSerde()));
+        Instant time = ZonedDateTime.now().toInstant();
 
-    testDriver = new TopologyTestDriver(builder.build(), props);
+        inputTopic.pipeInput("a", 5, time);
+        inputTopic.pipeInput("b", 2, time);
+        inputTopic.pipeInput("a", 3, time);
 
-    inputTopic = testDriver.createInputTopic(INPUT_TOPIC, new StringSerializer(), new IntegerSerializer());
-    outputTopic = testDriver.createOutputTopic(OUTPUT_TOPIC, new StringDeserializer(), new IntegerDeserializer());
+        time = time.plusMillis(100);
 
-  }
+        inputTopic.pipeInput("a", 9, time);
+        inputTopic.pipeInput("b", 11, time);
 
-  @AfterEach
-  public void tearDown() {
-    testDriver.close();
-  }
+        time = time.plusSeconds(10);
 
-  @Test
-  public void shouldWindow() {
+        inputTopic.pipeInput("c", 9, time);
+        inputTopic.pipeInput("c", 11, time);
+        inputTopic.pipeInput("b", 5, time);
+        inputTopic.pipeInput("b", 9, time);
 
-    Instant time = ZonedDateTime.now().toInstant();
+        time = time.plusSeconds(4);
+        inputTopic.pipeInput("dump", 1, time);
 
-    inputTopic.pipeInput("a", 5, time);
-    inputTopic.pipeInput("b", 2, time);
-    inputTopic.pipeInput("a", 3, time);
+        assertThat(outputTopic.getQueueSize()).isEqualTo(4);
 
-    time = time.plusMillis(100);
+        List<KeyValue<String, Integer>> a = Arrays.asList(
+                new KeyValue<>("a", 3),
+                new KeyValue<>("b", 2),
+                new KeyValue<>("b", 2),
+                new KeyValue<>("c", 2)
+        );
 
-    inputTopic.pipeInput("a", 9, time);
-    inputTopic.pipeInput("b", 11, time);
+        assertThat(outputTopic.readKeyValuesToList()).isEqualTo(a);
 
-    time = time.plusSeconds(10);
-
-    inputTopic.pipeInput("c", 9, time);
-    inputTopic.pipeInput("c", 11, time);
-    inputTopic.pipeInput("b", 5, time);
-    inputTopic.pipeInput("b", 9, time);
-
-    time = time.plusSeconds(4);
-    inputTopic.pipeInput("dump", 1, time);
-
-    assertThat(outputTopic.getQueueSize()).isEqualTo(4);
-
-    List<KeyValue<String, Integer>> a = Arrays.asList(
-      new KeyValue<>("a", 3),
-      new KeyValue<>("b", 2),
-      new KeyValue<>("b", 2),
-      new KeyValue<>("c", 2)
-    );
-
-    assertThat(outputTopic.readKeyValuesToList()).isEqualTo(a);
-
-  }
+    }
 
 }
